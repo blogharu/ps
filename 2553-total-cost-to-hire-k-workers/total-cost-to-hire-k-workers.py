367059e194784315172e988d51e1d41ac7899d2e
blogharu <34163093+blogharu@users.noreply.github.com>
2023-08-06 10:35:20 -0700

    Time: 647 ms (99.69%) | Memory: 27.9 MB (34.30%) - LeetSync

--- 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.py
+++ 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.py
@@ -3,6 +3,9 @@ from collections import deque
 
 class Solution:
     def totalCost(self, costs: List[int], k: int, candidates: int) -> int:
+        if len(costs) == k:
+            return sum(costs)
+
         answer = 0
         costs = deque(costs)
         heapl, heapr = [], []
@@ -14,28 +17,17 @@ class Solution:
             elif costs:
                 heappush(heapl, costs.popleft())
         
-        while k and costs:
-            if heapl[0] == heapr[0]:
+        for _ in range(k):
+            if costs and heapl[0] == heapr[0]:
                 is_left = costs[0] <= costs[-1]
             else:
                 is_left =  heapl[0] < heapr[0]
             if is_left:
                 answer += heappop(heapl)
-                heappush(heapl, costs.popleft())
+                heappush(heapl, costs.popleft() if costs else 10**6)
             else:
                 answer += heappop(heapr)
-                heappush(heapr, costs.pop())
+                heappush(heapr, costs.pop() if costs else 10**6)
             k -= 1
         
-        while k and heapl and heapr:
-            if heapl[0] <= heapr[0]:
-                answer += heappop(heapl)
-            else:
-                answer += heappop(heapr)
-            k -= 1
-
-        heap = heapl if heapl else heapr
-        for _ in range(k):
-            answer += heappop(heap)
-
         return answer
