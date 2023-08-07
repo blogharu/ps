dd9a60b6e8a8d03fc5927c56cfe154abc4d8be1d
blogharu <34163093+blogharu@users.noreply.github.com>
2023-08-07 16:58:20 -0700

    Time: 2289 ms (25.22%) | Memory: 626 MB (5.01%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,15 @@
+from functools import cache
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        @cache
+        def get_answer(i, k, is_stock):
+            if i == len(prices):
+                return 0
+            answer = get_answer(i+1, k, is_stock)
+            if is_stock:
+                answer = max(answer, get_answer(i+1, k, not is_stock)+prices[i])
+            elif k:
+                answer = max(answer, get_answer(i+1, k-1, not is_stock)-prices[i])
+            return answer
+        return get_answer(0, 2, False)
