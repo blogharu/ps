fb79b19e5f47512afe204c83093cf428e378d236
blogharu <34163093+blogharu@users.noreply.github.com>
2023-08-06 20:55:28 -0700

    Time: 1080 ms (17.31%) | Memory: 258.6 MB (5.45%) - LeetSync

---
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -0,0 +1,16 @@
+from functools import cache
+
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        @cache
+        def get_answer(i, has_stock):
+            answer = 0
+            if i != len(prices):
+                answer = get_answer(i+1, has_stock)
+                if has_stock:
+                    answer = max(answer, prices[i] - fee + get_answer(i+1, not has_stock))
+                else:
+                    answer = max(answer, -prices[i] + get_answer(i+1, not has_stock))
+            return answer
+        return get_answer(0, False)
+        
