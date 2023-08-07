7378aec02bd9d770f9d892f8102641ca5afe750b
blogharu <34163093+blogharu@users.noreply.github.com>
2023-08-06 20:39:52 -0700

    Time: 686 ms (84.64%) | Memory: 91.4 MB (17.71%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        cache = defaultdict(int)
+        def get_answer(i, j):
+            if (i, j) not in cache and not (i == len(text1) or j == len(text2)):
+                c1, c2 = text1[i], text2[j]
+                answer = get_answer(i+1, j+1)
+                if c1 == c2:                
+                    answer += 1
+                else:
+                    try:
+                        next_j = text2.index(c1, j+1)
+                    except:
+                        ...
+                    else:
+                        answer = max(answer, 1 + get_answer(i+1, next_j+1))
+                    try:
+                        next_i = text1.index(c2, i+1)
+                    except:
+                        ...
+                    else:
+                        answer = max(answer, 1 + get_answer(next_i+1, j+1))
+                cache[(i,j)] = answer
+            return cache[(i,j)]
+        return get_answer(0, 0)
