34e18d75efb3c9440c5e3bc81afc9324b6ec7883
blogharu <34163093+blogharu@users.noreply.github.com>
2023-08-09 13:21:26 -0700

    Time: 467 ms (36.24%) | Memory: 17.1 MB (99.81%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -1,11 +1,9 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        answers = {0:0}
-        for i in range(len(nums)):
-            answer = answers[i]
-            for j in range(i+1, i+1+nums[i]):
-                if j in answers:
-                    answers[j] = min(answers[j],answer + 1)             
-                else:
-                    answers[j] = answer + 1
-        return answers[len(nums)-1]
+        nums[-1] = 0
+        for i in range(len(nums)-2, -1, -1):
+            if nums[i]:
+                nums[i] = 1+min(nums[i+1:i+1+nums[i]])
+            else:
+                nums[i] = len(nums)
+        return nums[0]
