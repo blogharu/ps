587e505b6a2fa06fa927dd4fb0c6bcd52ce3201e
blogharu <34163093+blogharu@users.noreply.github.com>
2023-08-11 03:33:53 -0700

    Time: 916 ms (88.59%) | Memory: 18.1 MB (81.43%) - LeetSync

--- 212-word-search-ii/word-search-ii.py
+++ 212-word-search-ii/word-search-ii.py
@@ -4,11 +4,6 @@ class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         answers = []
 
-        board = {(i,j):c for i, s in enumerate(board) for j, c in enumerate(s)}
-        cs = defaultdict(set)
-        for k, v in board.items():
-            cs[v].add(k)
-
         trie = {}
         for word in words:
             temp = trie
@@ -27,22 +22,24 @@ class Solution:
                 del trie[True]
             for (dx, dy) in ds:
                 nx, ny = x+dx, y+dy
-                if (nx, ny) not in stack and (nx, ny) in board:
-                    c = board[(nx, ny)]
+                if (nx, ny) not in stack and 0<=nx<len(board) and 0<=ny<len(board[0]):
+                    c = board[nx][ny]
                     if c in trie:
                         answer.append(c)
                         stack.add((nx, ny))
                         dfs(trie[c], nx, ny)
                         stack.remove((nx, ny))
                         answer.pop()
+                        if len(trie[c]) == 0:
+                            del trie[c]
             return False
         
-        for c in trie:
-            for pos in cs.get(c, []):
-                answer.append(c)
-                stack.add(pos)
-                dfs(trie[c], *pos)
-                stack.remove(pos)
-                answer.pop()
-
+        for i, row in enumerate(board):
+            for j, c in enumerate(row):
+                if c in trie:
+                    answer.append(c)
+                    stack.add((i, j))
+                    dfs(trie[c], i, j)
+                    stack.remove((i, j))
+                    answer.pop()
         return answers
