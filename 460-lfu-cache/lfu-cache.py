af188332114db19b8be8c87174d9d541af72314f
blogharu <34163093+blogharu@users.noreply.github.com>
2023-08-11 11:52:47 -0700

    Time: 680 ms (93.30%) | Memory: 83 MB (7.42%) - LeetSync

---
+++ 460-lfu-cache/lfu-cache.py
@@ -0,0 +1,38 @@
+from heapq import heappush, heappop
+
+class LFUCache:
+    def __init__(self, capacity: int):        
+        self._capacity = capacity
+        self._cache = {}
+        self._lfu = {}
+        self._lfu_heap = []
+        self._count = 0
+
+    def _update(self, key):
+        self._lfu[key] += 1
+        heappush(self._lfu_heap, (self._lfu[key], self._count, key))
+        self._count += 1
+
+    def get(self, key: int) -> int:        
+        val = self._cache.get(key, -1)
+        if val >= 0:
+            self._update(key)
+        return self._cache.get(key, -1)
+
+    def put(self, key: int, value: int) -> None:
+        if key in self._cache:
+            self._cache[key] = value
+            self._update(key)
+        else:
+            if len(self._cache) == self._capacity:
+                while self._lfu_heap:
+                    f, _, c = heappop(self._lfu_heap)
+                    if f == self._lfu[c]:                
+                        break
+                del self._lfu[c]
+                del self._cache[c]
+            self._lfu[key] = 1
+            heappush(self._lfu_heap, (1, self._count, key))
+            self._count += 1
+            self._cache[key] = value
+        
