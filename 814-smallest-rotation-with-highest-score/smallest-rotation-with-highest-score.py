2ef572fc06e25f8cfc7536879cd4e9c33e60611c
blogharu <34163093+blogharu@users.noreply.github.com>
2023-08-11 14:18:17 -0700

    Time: 700 ms (96.15%) | Memory: 30.3 MB (42.31%) - LeetSync

--- 814-smallest-rotation-with-highest-score/smallest-rotation-with-highest-score.py
+++ 814-smallest-rotation-with-highest-score/smallest-rotation-with-highest-score.py
@@ -1,21 +1,8 @@
-from heapq import heappush, heappop
-
 class Solution:
-    def bestRotation(self, nums: List[int]) -> int:
-        heap = []
-        for i, num in enumerate(nums):
-            if num > i:
-                heappush(heap, num-i)
-
-        max_score, answer = -len(heap), 0
-
-        for i in range(1, len(nums)):
-            heappush(heap, nums[-i]+i)
-            while heap and heap[0] == i:
-                heappop(heap)
-            if -len(heap) > max_score:
-                max_score, answer = -len(heap), len(nums) - i    
-            elif -len(heap) == max_score:
-                answer = min(len(nums) - i, answer)
-
-        return answer
+    def bestRotation(self, A):
+        n = len(A)
+        P = [1] * n
+        for i in range(n):
+            P[(i - A[i] + 1) % n] -= 1
+        P = list(accumulate(P))
+        return P.index(max(P))
