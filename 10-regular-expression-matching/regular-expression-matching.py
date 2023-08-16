d11a01b24b6e50b536c654b61288d53d5d94e039
blogharu <34163093+blogharu@users.noreply.github.com>
2023-08-15 23:54:43 -0700

    Time: 9718 ms (5.15%) | Memory: 16.4 MB (55.03%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.py
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -1,5 +1,3 @@
-from functools import cache
-
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         queries = []
@@ -15,7 +13,6 @@ class Solution:
                 temp.append(c)
         if temp:
             queries.append("".join(temp))
-        @cache
         def get_answer(si, qi):
             if qi == len(queries):
                 return si == len(s) and qi == len(queries)
