965f68690b6b7b28a6718068f3d29a22340fc41d
blogharu <34163093+blogharu@users.noreply.github.com>
2023-08-21 20:26:47 -0700

    Time: 464 ms (36.60%) | Memory: 17 MB (99.79%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -1,23 +1,9 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        length, step = len(nums), 0
-        if length <= 1:
-            return 0
-        if nums[0] >= length - 1:
-            return 1
-        dp = []
-        dp.append((0, nums[0]))
-        step = 1
-        while dp[0][0] < length - 1:
-            idx, num = dp.pop()
-            max_idx, temp = 0, 0
-            for cur_idx, cur_num in enumerate(nums[idx + 1: idx + num + 1]):
-                if cur_num + cur_idx > temp:
-                    temp = cur_num + cur_idx
-                    max_idx = cur_idx
-            # print(f"{max_idx + idx + 1}, {nums[max_idx + idx + 1]}")
-            dp.append((max_idx + idx + 1, nums[max_idx + idx + 1]))
-            step += 1
-            if max_idx + idx + 1 + nums[max_idx + idx + 1] >= length - 1:
-                break
-        return step
+        nums[-1] = 0
+        for i in range(len(nums)-2, -1, -1):
+            if nums[i]:
+                nums[i] = 1+min(nums[i+1:i+1+nums[i]])
+            else:
+                nums[i] = len(nums)
+        return nums[0]
