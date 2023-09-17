74c6e9410365c2b4aa1508741b88febad17f87a8
blogharu <34163093+blogharu@users.noreply.github.com>
2023-09-17 08:39:35 -0700

    Time: 232 ms (39.69%) | Memory: 27.2 MB (19.69%) - LeetSync

--- 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.py
+++ 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.py
@@ -1,21 +1,16 @@
+from collections import deque
+
 class Solution:
     def shortestPathLength(self, graph: List[List[int]]) -> int:
-        answer = 0
-        nodes = {(1<<i, i) for i in range(len(graph))}
-        next_nodes = set()
+        nodes = deque((1<<i, i, 0) for i in range(len(graph)))
         target = (1 << len(graph)) - 1
-        done = {}
+        done = set()
         while nodes:
-            node = nodes.pop()
-            if node not in done:
-                done[node] = answer
-                for next_node in graph[node[1]]:
-                    next_mask = node[0] | (1<<next_node)
-                    if next_mask == target:
-                        return answer+1
-                    next_nodes.add((node[0] | 1<<next_node, next_node))
-            if not nodes:
-                nodes = next_nodes
-                next_nodes = set()
-                answer += 1 
-        return 0
+            mask, node, answer = nodes.popleft()
+            if (mask, node) not in done:
+                done.add((mask, node))
+                if mask == target:
+                    return answer
+                answer += 1
+                for next_node in graph[node]:
+                    nodes.append((mask | 1<<next_node, next_node, answer))
