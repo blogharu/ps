180729056d330c3da65f0fa1bf18f69bcbaa42b0
blogharu <34163093+blogharu@users.noreply.github.com>
2023-10-10 21:41:58 -0700

    Time: 1513 ms (21.83%) | Memory: 41 MB (8.26%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,16 +1,19 @@
+# 9:32
+# 
+
 from heapq import heappush, heappop
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        if len(nums) <= k:
-            return [max(nums)]
         answer = []
         heap = []
-        for i in range(k-1):
+        for i in range(k):
             heappush(heap, (-nums[i], i))
-        for i, num in enumerate(nums[k-1:], k-1):
-            heappush(heap, (-num, i))
-            while heap[0][1] <= i - k:
+        answer.append(-heap[0][0])
+        for i, num in enumerate(nums[k:]):
+            while heap and heap[0][1] <= i:
                 heappop(heap)
+            heappush(heap, (-num, i+k))
             answer.append(-heap[0][0])
+
         return answer
