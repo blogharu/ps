ce9c8e6c1e0fe6947b5a9c4e4b1fd85e948f437a
blogharu <34163093+blogharu@users.noreply.github.com>
2023-10-11 06:47:33 -0700

    Time: 909 ms (68.49%) | Memory: 44 MB (67.88%) - LeetSync

--- 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.py
+++ 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.py
@@ -1,22 +1,19 @@
-from collections import deque
 from heapq import heappush, heappop
 
 class Solution:
     def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
         people_dict = {p:0 for p in people}
         people_keys = sorted(people_dict.keys())
-        flowers = deque(sorted((start, end) for (start, end) in flowers))
+        flowers = sorted((start, end) for (start, end) in flowers)
+        f = 0
         heap = []
         for p in people_keys:
             while heap and heap[0] < p:
                 heappop(heap)
-            while flowers:
-                if flowers[0][0] > p:
-                    break
-                elif flowers[0][1] < p:
-                    flowers.popleft()
-                else:
-                    heappush(heap, flowers.popleft()[1])
+            while f < len(flowers) and flowers[f][0] <= p:
+                if flowers[f][1] >= p:
+                    heappush(heap, flowers[f][1])
+                f += 1
             people_dict[p] = len(heap)            
         return [people_dict[p] for p in people]
         
