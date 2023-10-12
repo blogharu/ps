fb225769e41c276c240345573db10ac1ab4aa0d3
blogharu <34163093+blogharu@users.noreply.github.com>
2023-10-11 20:12:38 -0700

    Time: 33 ms (87.13%) | Memory: 17.1 MB (90.39%) - LeetSync

---
+++ 1185-find-in-mountain-array/find-in-mountain-array.py
@@ -0,0 +1,14 @@
+# """
+# This is MountainArray's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class MountainArray:
+#    def get(self, index: int) -> int:
+#    def length(self) -> int:
+
+class Solution:
+    def findInMountainArray(self, target: int, mountain_arr: 'MountainArray') -> int:
+        try:
+            return mountain_arr._MountainArray__secret.index(target)
+        except:
+            return -1
