013cae7cd970a63360aa34735cdd842de802e0d1
blogharu <34163093+blogharu@users.noreply.github.com>
2023-10-12 05:54:21 -0700

    Time: 1983 ms (68.12%) | Memory: 18.5 MB (5.85%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -3,11 +3,7 @@ from functools import cache
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         @cache
-        def get_answer(i, last):
-            temp = 0
-            for x in range(i+1, len(nums)):
-                if last < nums[x]:
-                    temp = max(temp, get_answer(x, nums[x]))
-            return 1 + temp
-        return max(get_answer(i, nums[i]) for i in range(len(nums)-1, -1, -1))
+        def get_answer(i):
+            return 1 + max(0, 0, *list(get_answer(j) for j in range(i+1, len(nums)) if nums[i] < nums[j]))
+        return max(get_answer(i) for i in range(len(nums)-1, -1, -1))
         
