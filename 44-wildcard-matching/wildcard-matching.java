e71ff68b1af438959e162c03ed14c34a039517a8
blogharu <34163093+blogharu@users.noreply.github.com>
2023-10-13 09:03:08 -0700

    Time: 6 ms (89.52%) | Memory: 43.3 MB (94.98%) - LeetSync

---
+++ 44-wildcard-matching/wildcard-matching.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int si = 0, pi = 0, ppi = -1, ssi = -1;
+        while (si < s.length()) {
+            if (pi < p.length() && (p.charAt(pi) == '?' || p.charAt(pi) == s.charAt(si))) {
+                pi++;
+                si++;
+            }
+            else if (pi < p.length() && p.charAt(pi) == '*') {
+                ppi = ++pi;
+                ssi = si;
+            }
+            else if (ppi == -1) { return false; }
+            else {
+                pi = ppi;
+                si = ssi++;
+            }
+        }
+        return pi == p.length() || p.substring(pi).length() == p.substring(pi).chars().filter(c -> c == '*').count();
+    }
+}
