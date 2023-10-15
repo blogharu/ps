4e02332481e321baf6f687b028b3c3de345ce2e5
blogharu <34163093+blogharu@users.noreply.github.com>
2023-10-15 11:14:33 -0700

    Time: 626 ms (50.58%) | Memory: 38.7 MB (46.60%) - LeetSync

--- 2765-make-array-empty/make-array-empty.py
+++ 2765-make-array-empty/make-array-empty.py
@@ -1,16 +1,14 @@
 class Solution:
-    def countOperationsToEmptyArray(self, nums_origin: List[int]) -> int:
-        answer = len(nums_origin)
-        bit_mask = 1 << len(nums_origin) * 2
-        base = (1 << len(nums_origin)) + 1
-        nums = sorted([(num, i) for i, num in enumerate(nums_origin)])
-        prev = 0
-        if nums[0][1] == 0:
-            nums.pop(0) 
-            bit_mask |= base
-        for (num, i) in nums:
-            ii = i if prev < i else i + len(nums_origin)
-            answer += ii - prev - ((bit_mask >> prev) & ((1 << (ii-prev+1)) - 1)).bit_count()
-            bit_mask |= base << i
-            prev = i
-        return answer
+    def countOperationsToEmptyArray(self, nums: List[int]) -> int:
+        mapping = {}
+        for i, num in enumerate(nums):
+            mapping[num] = i
+        nums.sort()
+        # best case requires n steps
+        n = len(nums)
+        result = n
+
+        for i in range(1, n):
+            if mapping[nums[i]] < mapping[nums[i-1]]:
+                result += n - i
+        return result
