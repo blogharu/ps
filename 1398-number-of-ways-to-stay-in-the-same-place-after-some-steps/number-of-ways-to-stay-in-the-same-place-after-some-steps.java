8279eecdd9def6734ac11d4edfa231f3fb7c2623
blogharu <34163093+blogharu@users.noreply.github.com>
2023-10-15 01:06:31 -0700

    Time: 15 ms (36.59%) | Memory: 47.4 MB (27.44%) - LeetSync

--- 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.java
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.java
@@ -1,20 +1,16 @@
 class Solution {
-    int MOD = 1000000007;
     public int numWays(int steps, int arrLen) {
         int[][] counts = new int[2][arrLen];
-        int cur = 0;
-        counts[cur][0] = 1;
+        counts[0][0] = 1;
         for (int i = 0; i < steps; i++) {
-            int next = (cur+1)%2;
             for (int j = 0; j < Math.min(arrLen, steps-i); j++) {
-                counts[next][j] = counts[cur][j];
+                counts[(i+1)%2][j] = counts[i%2][j];
                 if (j > 0) 
-                    counts[next][j] = (counts[next][j]+counts[cur][j-1]) % MOD;
+                    counts[(i+1)%2][j] = (counts[(i+1)%2][j]+counts[i%2][j-1]) % 1000000007;
                 if (j < arrLen-1) 
-                    counts[next][j] = (counts[next][j]+counts[cur][j+1]) % MOD;                
+                    counts[(i+1)%2][j] = (counts[(i+1)%2][j]+counts[i%2][j+1]) % 1000000007;                
             }
-            cur = next;
         }
-        return counts[cur][0];
+        return counts[steps%2][0];
     }
 }
