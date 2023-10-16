e74d986e9f208287ede91f26694110e218924a54
blogharu <34163093+blogharu@users.noreply.github.com>
2023-10-16 10:06:35 -0700

    Time: 0 ms (100.00%) | Memory: 43.4 MB (88.35%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,28 +1,30 @@
 class Solution {
+    int[] nums;
     List<List<Integer>> ans = new ArrayList<>();
     public List<List<Integer>> permute(int[] nums) {
-        function(nums, 0);
+        this.nums = nums;
+        function(0);
         return ans;
     }
 
-    public void function(int[] arr, int start) {
-        if (start == arr.length) {
+    public void function(int start) {
+        if (start == nums.length) {
             List<Integer> list = new ArrayList();
-            for (int i = 0; i < arr.length; i++) list.add(arr[i]);
+            for (int i = 0; i < nums.length; i++) list.add(nums[i]);
             ans.add(list);
             return;
         }
 
-        for (int i = start; i < arr.length; i++) {
-            swap(arr, start, i);
-            function(arr, start + 1);
-            swap(arr, start, i);
+        for (int i = start; i < nums.length; i++) {
+            swap(start, i);
+            function(start + 1);
+            swap(start, i);
         }
     }
 
-    public void swap(int[] arr, int a, int b) {
-        int temp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = temp;
+    public void swap(int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
     }
 }
