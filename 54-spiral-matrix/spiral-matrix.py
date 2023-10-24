f270d619005691bf3cbe664fc91d5eb1fcf67586
blogharu <34163093+blogharu@users.noreply.github.com>
2023-10-24 09:34:24 -0700

    Time: 32 ms (90.33%) | Memory: 16.3 MB (59.35%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,22 +1,22 @@
-# 2:48
-
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         matrix_dict = {}
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                matrix_dict[(i,j)] = matrix[i][j]
+        for i, row in enumerate(matrix):
+            for j, val in enumerate(row):
+                matrix_dict[(i, j)] = val
+
         answer = []
-        ds = [(0,1),(1,0),(0,-1),(-1,0)]
-        d_index = 0
-        node = (0,0)
-        while matrix_dict:
+
+        node = (0, 0)
+        prev_d = 0
+        ds = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        while node in matrix_dict:
             answer.append(matrix_dict.pop(node))
-            x, y = node
             for i in range(4):
-                nx, ny = x+ds[d_index][0], y+ds[d_index][1]
-                if (nx, ny) in matrix_dict:
-                    break
-                d_index = (d_index+1)%4                
-            node = (nx, ny)
+                d = (prev_d+i)%4
+                next_node = (node[0] + ds[d][0], node[1] + ds[d][1])
+                if next_node in matrix_dict:
+                    node = next_node
+                    prev_d = d
+        
         return answer
