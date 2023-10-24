22cd5f9967907e5a5b62a74ca2d2889ffeef77d5
blogharu <34163093+blogharu@users.noreply.github.com>
2023-10-24 09:48:46 -0700

    Time: 45 ms (85.78%) | Memory: 16.2 MB (53.66%) - LeetSync

--- 728-self-dividing-numbers/self-dividing-numbers.py
+++ 728-self-dividing-numbers/self-dividing-numbers.py
@@ -2,11 +2,11 @@ class Solution:
     def selfDividingNumbers(self, left: int, right: int) -> List[int]:
         answer = []
         for num in range(left, right+1):
-            is_answer = True
-            ds = set(str(num))
-            if "0" not in ds:
-                for d in set(str(num)):
-                    if num % int(d) != 0:
+            digits = set(str(num))
+            if "0" not in digits:
+                is_answer = True
+                for str_digit in set(str(num)):
+                    if num % int(str_digit) != 0:
                         is_answer = False
                         break
                 if is_answer:
