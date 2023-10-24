d101458d588c0ab5852d37f56038d59b4899815b
blogharu <34163093+blogharu@users.noreply.github.com>
2023-10-24 10:04:43 -0700

    Time: 43 ms (28.81%) | Memory: 17.2 MB (5.18%) - LeetSync

--- 306-additive-number/additive-number.py
+++ 306-additive-number/additive-number.py
@@ -1,22 +1,20 @@
 from functools import cache
+
 class Solution:
     def isAdditiveNumber(self, str_num: str) -> bool:
         @cache
-        def str_num_to_int(start, end) -> int:
-            # return num if it is possible number
-            # If str_num has leading zeros, return -1            
+        def get_num(start, end):
             num = int(str_num[start:end])
-            return num if end - start == len(str(num)) else -1
+            return num if len(str(num)) == end-start else -1
 
         @cache
         def is_additive_number(s0, s1, s2, end):
-            answer = False
-            num0 = str_num_to_int(s0,s1)
-            num1 = str_num_to_int(s1,s2)
-            num2 = str_num_to_int(s2,end)
+            num0 = get_num(s0, s1)
+            num1 = get_num(s1, s2)
+            num2 = get_num(s2, end)
             if num0 >= 0 and num1 >= 0 and num2 >= 0 and num0 + num1 == num2:
-                answer = True
-            return answer
+                return True
+            return False
         
         @cache
         def is_additive_sequence(s0, s1, s2, end):
@@ -33,6 +31,5 @@ class Solution:
             for s2 in range(s1+1, len(str_num)+1):
                 for end in range(s2+1, len(str_num)+1):
                     if is_additive_sequence(s0, s1, s2, end):
-                        return True        
+                        return True
         return False
-        
