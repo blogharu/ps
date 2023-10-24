6093d2668ca036b4f309b0bb4cbebb2cd0eeedd9
blogharu <34163093+blogharu@users.noreply.github.com>
2023-10-24 09:27:01 -0700

    Time: 50 ms (7.77%) | Memory: 17.3 MB (5.18%) - LeetSync

--- 306-additive-number/additive-number.py
+++ 306-additive-number/additive-number.py
@@ -1,22 +1,38 @@
 from functools import cache
-
 class Solution:
-    def isAdditiveNumber(self, num: str) -> bool:
+    def isAdditiveNumber(self, str_num: str) -> bool:
         @cache
-        def is_answer(v1, v2, i):
-            if i == len(num):
-                return True
-            elif num[i] == "0":
-                if v1 + v2 != 0:
-                    return False
-            v3 = v1+v2
-            if num[i:].startswith(str(v3)):
-                return is_answer(v2, v3, i+len(str(v3)))
+        def str_num_to_int(start, end) -> int:
+            # return num if it is possible number
+            # If str_num has leading zeros, return -1            
+            num = int(str_num[start:end])
+            return num if end - start == len(str(num)) else -1
+
+        @cache
+        def is_additive_number(s0, s1, s2, end):
+            answer = False
+            num0 = str_num_to_int(s0,s1)
+            num1 = str_num_to_int(s1,s2)
+            num2 = str_num_to_int(s2,end)
+            if num0 >= 0 and num1 >= 0 and num2 >= 0 and num0 + num1 == num2:
+                answer = True
+            return answer
+        
+        @cache
+        def is_additive_sequence(s0, s1, s2, end):
+            if is_additive_number(s0, s1, s2, end):
+                if end == len(str_num):
+                    return True
+                for next_end in range(end+1, len(str_num)+1):
+                    if is_additive_sequence(s1, s2, end, next_end):
+                        return True
             return False
-        for i in range(1, len(num)):
-            if len(num[:i]) == len(str(int(num[:i]))):
-                for j in range(i+1, len(num)):
-                    if len(num[i:j]) == len(str(int(num[i:j]))):
-                        if is_answer(int(num[:i]), int(num[i:j]), j):
-                            return True
+        
+        s0 = 0
+        for s1 in range(s0+1, len(str_num)+1):
+            for s2 in range(s1+1, len(str_num)+1):
+                for end in range(s2+1, len(str_num)+1):
+                    if is_additive_sequence(s0, s1, s2, end):
+                        return True        
         return False
+        
