00b80e18fd664c180121c468fc223672fec2c26b
blogharu <34163093+blogharu@users.noreply.github.com>
2023-12-17 19:22:15 -0800

    Time: 152 ms (78.71%) | Memory: 17.7 MB (71.41%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.py
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.py
@@ -2,12 +2,5 @@ from heapq import heappush, heappop
 
 class Solution:
     def maxProductDifference(self, nums: List[int]) -> int:
-        maxs = []
-        mins = []
-        for num in nums:
-            heappush(maxs, num)
-            heappush(mins, -num)
-            if len(maxs) > 2:
-                heappop(maxs)
-                heappop(mins)
-        return maxs[0] * maxs[1] - mins[0] * mins[1]
+        nums.sort()
+        return nums[-1] * nums[-2] - nums[0] * nums[1]
