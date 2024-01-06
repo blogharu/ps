20b242494cbdf775a9e7784f08099a5493af809e
blogharu <34163093+blogharu@users.noreply.github.com>
2024-01-05 22:17:11 -0800

    Time: 671 ms (51.66%) | Memory: 96.2 MB (20.99%) - LeetSync

---
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+from bisect import bisect_left
+from functools import cache
+
+class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        jobs = defaultdict(list)
+        for start, end, value in zip(startTime, endTime, profit):
+            jobs[start].append((end,value))
+        starts = sorted(jobs.keys())
+        @cache
+        def get_answer(i):
+            if i == len(starts):
+                return 0
+            start = starts[i]
+            answer = get_answer(i+1)
+            for end, value in jobs[start]:
+                answer = max(answer, get_answer(bisect_left(starts, end)) + value)
+            return answer
+        return get_answer(0)
